19410ff7e2ee8b0f28aa3f4f3eff7f96ae65b093
Abhinav <abhinavgarg1218@gmail.com>
2018-02-18 12:24:37 +0530

    share

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,7 +2,7 @@ class Note < ActiveRecord::Base
   belongs_to :user
   has_many :tags, through: :taggings
   has_many :taggings, dependent: :destroy
-  has_many :shares, as: :shareable
+  has_many :shares, as: :shareable, dependent: :destroy
 
   def all_tags=(names)
   	self.tags = names.split(",").map do |name|
@@ -12,9 +12,5 @@ class Note < ActiveRecord::Base
 
 	def all_tags
 	  self.tags.map(&:name).join(", ")
-	end
-
-	def self.tag_with(name)
-  	Tag.find_by_name!(name).notes
 	end
 end
--- app/models/share.rb
+++ app/models/share.rb
@@ -1,6 +1,6 @@
 class Share < ActiveRecord::Base
   belongs_to :shareable, polymorphic: true
-  has_many :shares, as: :shareable
+  has_many :shares, as: :shareable, dependent: :destroy
   belongs_to :user
 
   enum permission: [ :reader, :updater, :owner ]
